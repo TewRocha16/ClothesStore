29248222eec512e2844bd5efbd5216dfe184df2b
TewRocha16 <mateusro.fonseca@gmail.com>
2023-12-05 19:01:33 -0300

    feat: interactions with seller and mirror

---
+++ Assets/ClotheStore/Scripts/Behaviours/Interactables/MirrorInteraction.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorInteraction : InteractionBase
+{
+    public override void Interact()
+    {
+        OpenChangeClothesUI();
+    }
+    public void OpenChangeClothesUI()
+    {
+        print("abri o espelho");
+    }
+}
